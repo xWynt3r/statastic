7f2403c80263445b82de64ef611602341ddd99a9
Culv3r <danny.r.larsen@gmail.com>
2020-07-16 20:05:29 -0500

    Update Dependencies

--- backend/statastic/settings.py
+++ backend/statastic/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+import environ
 
-import os
+root = environ.Path(__file__) - 3  # get root of the project
+env = environ.Env()
+environ.Env.read_env()  # reading .env file
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
