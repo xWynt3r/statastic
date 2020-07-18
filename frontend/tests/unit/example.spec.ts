dba36685008f64c8919b5e35d7173296a18bb3a5
Culv3r <danny.r.larsen@gmail.com>
2020-07-17 21:44:09 -0500

    Update Test

--- frontend/tests/unit/example.spec.ts
+++ frontend/tests/unit/example.spec.ts
@@ -2,11 +2,8 @@ import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+  it('mounts the component', () => {
+    const wrapper = shallowMount(HelloWorld, {});
+    expect(wrapper.text.toString().includes("Welcome to Vuetify"));
   });
 });
